9c4fff2d6b994200bdd6875e01ce5f8ebf7b35fa
Prashant Sani <contact@prashantsani.com>
2020-02-24 18:50:24 +0530

    Updates

---
+++ precache-manifest.85e2c81e884af46a748bb37759143ffd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea9e55abe2afe5d17d4bd1a5144685fa",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "a066883077bc9102df92",
+    "url": "/react-tic-tac-toe/static/css/main.437073e2.chunk.css"
+  },
+  {
+    "revision": "d8b6ffee929070c962c8",
+    "url": "/react-tic-tac-toe/static/js/2.f473dba6.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-tic-tac-toe/static/js/2.f473dba6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a066883077bc9102df92",
+    "url": "/react-tic-tac-toe/static/js/main.3a65e84d.chunk.js"
+  },
+  {
+    "revision": "f67fddcf588b95e3209d",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.63526b18.js"
+  }
+]);
